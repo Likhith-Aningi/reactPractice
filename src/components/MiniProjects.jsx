51e99f5ddbaa6620c1c1701e9c8aef3135859e39
Likhith <likhith.a@way2news.com>
2024-04-08 11:05:19 +0530

    Digital Clock Done

--- src/components/MiniProjects.jsx
+++ src/components/MiniProjects.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import ColorPicker from './miniProjects/ColorPicker';
 import TodoList from './miniProjects/TodoList';
+import DigitalClock from './miniProjects/DigitalClock';
 
 function MiniProjects() {
     const [currentMiniProject,setCurrentMiniProject]=useState('colorPicker');
     const availableProjects={
         colorPicker:<ColorPicker />,
         todoList:<TodoList/>,
+        digitalClock:<DigitalClock/>,
     }
   return (
     <div style={{
@@ -18,6 +20,7 @@ function MiniProjects() {
         <select defaultValue={currentMiniProject} onChange={e=>setCurrentMiniProject(e.target.value)}>
             <option value='colorPicker'>color picker</option>
             <option value='todoList'>Todo App</option>
+            <option value='digitalClock'>Digital Clock</option>
         </select>
         <div style={{padding:"40px"}}>
             {availableProjects[currentMiniProject]}
---
+++ src/components/miniProjects/DigitalClock.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+
+function DigitalClock() {
+    const [time, setTime] = useState(new Date());
+    function formatTime() {
+        const hours = time.getHours();
+        const minutes = time.getMinutes();
+        const seconds = time.getSeconds();
+        const meridiem = hours < 12 ? 'AM' : 'PM'
+        return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`;
+    }
+    function padZero(num) {
+        return (num < 10 ? '0' : '') + num;
+    }
+    useEffect(()=>{
+        const timeUpdate=setInterval(()=>{
+            setTime(new Date());
+        },1000);
+        return ()=>{
+            //CLEAN UP WHEN COMPONENT IS DISMOUNTED OR RE-RENDERED
+            clearInterval(timeUpdate);
+        }
+    })
+    return (
+        <div className='digital-clock-container'>
+            <h3>Digital Clock</h3>
+            <div className='digital-clock'>{formatTime()}</div>
+        </div>
+    )
+}
+
+export default DigitalClock
