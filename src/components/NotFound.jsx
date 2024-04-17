90ddbdff1530c7b3d39edd99ffb45d11a3b57367
likhith <likhithaningi@gmail.com>
2024-04-17 13:23:51 +0530

    Added toaster and auto redirection for 404

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,6 +1,15 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 function NotFound() {
+  const nav = useNavigate();
+  useEffect(() => {
+    toast.warn("Navigating to / in 10 seconds");
+    const timeOut = setTimeout(() => nav("/"), 10000);
+    return () => {
+      clearTimeout(timeOut);
+    };
+  }, []);
   return (
     <div>
       <h1
@@ -13,6 +22,7 @@ function NotFound() {
       >
         Page Not found 404
       </h1>
+      <ToastContainer autoClose={false} closeButton={false} />
     </div>
   );
 }
--- src/components/demoComponents/Demo.jsx
+++ src/components/demoComponents/Demo.jsx
@@ -1,20 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { availableDemos } from "./AvailableDemos";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ThemeContext from "../ThemeContext";
 function Demo() {
   const { demo } = useParams();
   const nav = useNavigate();
-
+  const { theme } = useContext(ThemeContext);
+  const timeOutRef = useRef(null);
+  const notify = () =>
+    toast.info("Going back to /demos in 10sec", {
+      autoClose: 10000,
+      pauseOnHover: false,
+      theme: theme,
+      position: "bottom-right",
+      onClose: ()=>clearTimeout(timeOutRef.current),
+    });
   useEffect(() => {
     if (!availableDemos[demo]) {
-      const timeoutId = setTimeout(() => nav("/demos"), 10000);// redirecting to /demos after 10 secs
-      return () => clearTimeout(timeoutId);
+      notify();
+      timeOutRef.current = setTimeout(() => {
+        nav("/demos");
+      }, 10000); // redirecting to /demos after 10 secs
+      return () => clearTimeout(timeOutRef.current);
     }
   }, [demo, nav]);
 
   if (!availableDemos[demo]) {
-    return <h2>NO Component like that yet brother</h2>;
+    return (
+      <div>
+        <ToastContainer />
+        <h2>NO Component like that yet brother</h2>
+      </div>
+    );
   }
 
   return (
